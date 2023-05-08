e3e81aac89af93ce2202f65b6e543e3135cec9d5
Equinox766 <equinox766@gmail.com>
2023-05-08 17:40:31 -0400

    Refactoring Tests

--- tests/Feature/Articles/ListArticleTest.php
+++ tests/Feature/Articles/ListArticleTest.php
@@ -14,77 +14,27 @@ class ListArticleTest extends TestCase
 
     public function can_fetch_a_single_article()
     {
-         $this->withoutExceptionHandling();
 
          $article = Article::factory()->create();
 
          $response = $this->getJson(route('api.v1.articles.show', $article));
 
-         $response->assertExactJson([
-             'data' => [
-                 'type' => 'articles',
-                 'id'   => (string) $article->getRouteKey(),
-                 'attributes' => [
-                     'title' => $article->title,
-                     'slug' => $article->slug,
-                     'content' => $article->content
-                 ],
-                 'links' => [
-                    'self' => url('/api/v1/articles/'.$article->getRouteKey())
-                ]
-             ]
-
+         $response->assertJsonApiResource($article, [
+             'title' => $article->title,
+             'slug' => $article->slug,
+             'content' => $article->content
          ]);
     }
 
     /** @test */
     public function can_fetch_all_articles()
     {
-        $this->withoutExceptionHandling();
 
         $article = Article::factory()->count(3)->create();
         $response = $this->getJson(route('api.v1.articles.index'));
-        $response->assertJson([
-            'data' => [
-                [
-                    'type' => 'articles',
-                    'id'   => (string) $article[0]->getRouteKey(),
-                    'attributes' => [
-                        'title' => $article[0]->title,
-                        'slug' => $article[0]->slug,
-                        'content' => $article[0]->content
-                    ],
-                    'links' => [
-                        'self' => route('api.v1.articles.show', $article[0])
-                    ]
-                ],
-                ['type' => 'articles',
-                    'id'   => (string) $article[1]->getRouteKey(),
-                    'attributes' => [
-                        'title' => $article[1]->title,
-                        'slug' => $article[1]->slug,
-                        'content' => $article[1]->content
-                    ],
-                    'links' => [
-                        'self' => route('api.v1.articles.show', $article[1])
-                    ]
-                ],
-                [
-                    'type' => 'articles',
-                    'id'   => (string) $article[2]->getRouteKey(),
-                    'attributes' => [
-                        'title' => $article[2]->title,
-                        'slug' => $article[2]->slug,
-                        'content' => $article[2]->content
-                    ],
-                    'links' => [
-                        'self' => route('api.v1.articles.show', $article[2])
-                    ]
-                ]
-            ],
-            'links' => [
-                'self' => route('api.v1.articles.index')
-            ]
+
+        $response->assertJsonApiCollection($article, [
+            'title' , 'slug' , 'content'
         ]);
 
     }
