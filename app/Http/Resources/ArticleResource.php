e6d72895acde6f4a6388c1b4b6068263256d5d10
Equinox766 <equinox766@gmail.com>
2023-05-08 16:22:04 -0400

    Restructuring the resource to reuse the code

--- app/Http/Resources/ArticleResource.php
+++ app/Http/Resources/ArticleResource.php
@@ -6,6 +6,15 @@ use Illuminate\Http\Resources\Json\JsonResource;
 
 class ArticleResource extends JsonResource
 {
+    public function toJsonApi(): array
+    {
+       return [
+            'title'   => $this->resource->title,
+            'slug'    => $this->resource->slug,
+            'content' => $this->resource->content,
+        ];
+    }
+
     /**
      * Transform the resource into an array.
      *
@@ -15,35 +24,38 @@ class ArticleResource extends JsonResource
     public function toArray($request) : array
     {
         return [
-            'type' => 'articles',
+            'type' => $this->getResourceType(),
             'id'   => (string) $this->resource->getRouteKey(),
-            'attributes' => array_filter([
-                'title'   => $this->resource->title,
-                'slug'    => $this->resource->slug,
-                'content' => $this->resource->content
-            ], function ($value) {
-                if (request()->isNotFilled('fields')) {
-                    return $this;
-                }
-                $fields = explode(',', request('fields.articles'));
-
-                if($value === $this->getRouteKey()) {
-                    return in_array('slug', $fields);
-                }
-
-                return $value;
-            }),
+            'attributes' =>
+                $this->filterAttributes($this->toJsonApi()),
             'links' => [
-                'self' => url('/api/v1/articles/'.$this->resource->getRouteKey())
+                'self' => route('api.v1.'.$this->getResourceType().'.show', $this->resource)
             ]
         ];
     }
 
 
-    public function toResponse ($request)
+    public function withResponse ($request, $response)
     {
-        return parent::toResponse($request)->withHeaders([
-            'Location' => route('api.v1.articles.show', $this->resource)
-        ]);
+        $response->header(
+            'Location',
+            route('api.v1.'.$this->getResourceType().'.show', $this->resource)
+        );
+    }
+
+    public function filterAttributes(Array $attributes): array
+    {
+        return array_filter($attributes, function ($value) {
+            if (request()->isNotFilled('fields')) {
+                return $this;
+            }
+            $fields = explode(',', request('fields.'.$this->getResourceType()));
+
+            if($value === $this->getRouteKey()) {
+                return in_array($this->getRouteKeyName(), $fields);
+            }
+
+            return $value;
+        });
     }
 }
