48be738045fad5b99377b93c2c1d3a9dba63cf33
Equinox766 <equinox766@gmail.com>
2023-05-08 14:11:32 -0400

    Refactoring functions

--- app/Http/Controllers/Api/ArticleController.php
+++ app/Http/Controllers/Api/ArticleController.php
@@ -20,27 +20,12 @@ class ArticleController extends Controller
     public function index (Request $request): ArticleCollection
     {
 
-        $articles = Article::query();
+        $articles = Article::query()
+            ->allowedFilters(['title', 'content', 'month', 'year'])
+            ->allowedSorts(['title', 'content'])
+            ->jsonPaginate();
 
-        $allowedFilters = ['title', 'content', 'month', 'year'];
-        //filtro
-        foreach (request('filter', []) as $filter => $value) {
-
-            abort_unless(in_array($filter, $allowedFilters), 400);
-
-            if ($filter === 'year') {
-                $articles->whereYear('created_at', $value);
-            } else if ($filter === 'month') {
-                $articles->whereMonth('created_at', $value);
-            } else {
-                $articles->where($filter, 'LIKE', '%' . $value . '%');
-            }
-        }
-
-        //Se utiliza macro para ordenar con "allowedSorts" y para listar "jsonPaginate"
-        $articles->allowedSorts(['title', 'content']);
-
-        return ArticleCollection::make($articles->jsonPaginate());
+        return ArticleCollection::make($articles);
     }
 
     public function store (SaveArticleRequest $request): ArticleResource
---
+++ tests/Feature/Articles/SparseFieldsArticlesTest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Tests\Feature\Articles;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class SparseFieldsArticlesTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /** @test */
+
+    public function test_example()
+    {
+         //
+    }
+}
